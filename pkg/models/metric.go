557e68de259d00879975c6dbab7a6d5a9495e8df
xuebing <xuebing1110@gmail.com>
2018-01-05 16:01:22 +0800

    ignore cgroup fstype

--- pkg/models/metric.go
+++ pkg/models/metric.go
@@ -15,7 +15,7 @@ var (
 		Metric{"memoryUtilizationAvg", MetricType_Range, `100 - (avg_over_time(node_memory_MemAvailable{$filter}[$duration]) OR avg_over_time(node_memory_MemAvailable_ext{$filter}[$duration])) / node_memory_MemTotal{$filter} * 100`},
 		Metric{"memoryUtilizationMax", MetricType_Range, `100 - (max_over_time(node_memory_MemAvailable{$filter}[$duration]) OR max_over_time(node_memory_MemAvailable_ext{$filter}[$duration])) / node_memory_MemTotal{$filter} * 100`},
 		Metric{"memoryUtilizationMin", MetricType_Range, `100 - (min_over_time(node_memory_MemAvailable{$filter}[$duration]) OR min_over_time(node_memory_MemAvailable_ext{$filter}[$duration])) / node_memory_MemTotal{$filter} * 100`},
-		Metric{"diskUtilization", MetricType_Current, `100 - node_filesystem_free{$filter, fstype!~"rootfs|selinuxfs|autofs|rpc_pipefs|tmpfs|iso.+"} / node_filesystem_size * 100`},
+		Metric{"diskUtilization", MetricType_Current, `100 - node_filesystem_free{$filter, fstype!~"cgroup|rootfs|selinuxfs|autofs|rpc_pipefs|tmpfs|iso.+"} / node_filesystem_size * 100`},
 		Metric{"diskUtilizationMax", MetricType_Range, `100 - (max_over_time(node_filesystem_free{$filter, fstype!~"rootfs|selinuxfs|autofs|rpc_pipefs|tmpfs|iso.+"}[$duration])) / node_filesystem_size * 100`},
 	}
 
